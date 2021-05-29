0969d84a0d4dd3dfee56d34e7e0d9859adebb353
Islendingurinn <arnibent1312@hotmail.com>
2021-05-29 11:22:24 +0200

    Further misc routing changes and fixes

--- OxbridgeBackend/src/routes/v1/user/user.ts
+++ OxbridgeBackend/src/routes/v1/user/user.ts
@@ -14,7 +14,7 @@ import { RoleCode } from '../../../database/model/Role';
 const router = express.Router();
 
 // Below all APIs are private APIs protected for Access Token and Users Role
-router.use('/', authentication, role(RoleCode.USER), authorization);
+router.use('/', authentication, role(RoleCode.USER), role(RoleCode.ADMIN), authorization);
 // ---------------------------------------------------------------------------
 
 /**
